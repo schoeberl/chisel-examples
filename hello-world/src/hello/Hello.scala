835a8e820227a709e517da5d1679a38b6438685e
Martin Schoeberl <martin@jopdesign.com>
2014-02-06 18:51:14 +0100

    from source

--- hello-world/src/hello/Hello.scala
+++ hello-world/src/hello/Hello.scala
@@ -19,21 +19,26 @@ class Hello extends Module {
   val io = new Bundle {
     val led = UInt(OUTPUT, 1)
   }
-  val CNT_MAX = UInt(16000000/2-1);
+  val CNT_MAX = UInt(16000000 / 2 - 1);
   val r1 = Reg(init = UInt(0, 25))
   val blk = Reg(init = UInt(0, 1))
-  
-  r1 := r1 + UInt(1)  
-  when (r1 === CNT_MAX) {
+
+  r1 := r1 + UInt(1)
+  when(r1 === CNT_MAX) {
     r1 := UInt(0)
     blk := ~blk
   }
   io.led := blk
 }
-  
+
 // Generate the Verlog code by invoking chiselMain() in our main()
 object HelloMain {
-  def main(args: Array[String]): Unit = { 
-    chiselMain( args, () => Module(new Hello()))
+  def main(args: Array[String]): Unit = {
+    println("Hello World")
+    println("Nr args=" + args.length)
+    for (i <- 0 until args.length) {
+      println(args(i))
+    }
+    chiselMain(args, () => Module(new Hello()))
   }
 }
