ca753223068fef1df6f3fe7e16e50abd39ed6392
Martin Schoeberl <martin@jopdesign.com>
2015-12-11 18:50:00 -0800

    ALU: split between the ALU function und the FPGA IO mapping

--- examples/src/Alu.scala
+++ examples/src/Alu.scala
@@ -13,19 +13,19 @@ import scala.collection.mutable.HashMap
 
 /**
  * This is a very basic ALU example.
- *
- * This should be an ALU and not have switches
- * as inputs and leds as output.
  */
 class Alu extends Module {
   val io = new Bundle {
-    val sw = UInt(INPUT, 10)
-    val led = UInt(OUTPUT, 10)
+    val fn = UInt(INPUT, 2)
+    val a = UInt(INPUT, 4)
+    val b = UInt(INPUT, 4)
+    val result = UInt(OUTPUT, 4)
   }
 
-  val fn = io.sw(1, 0)
-  val a = io.sw(5, 2)
-  val b = io.sw(9, 6)
+  // Use shorter variable names
+  val fn = io.fn
+  val a = io.a
+  val b = io.b
 
   val result = UInt(width = 4)
   // some default value is needed
@@ -40,18 +40,41 @@ class Alu extends Module {
   }
 
   // Output on the LEDs (with zero extension)
-  io.led := result
+  io.result := result
+}
+
+/**
+ * A top level to wire FPGA buttons and LEDs
+ * to the ALU input and output.
+ */
+class AluTop extends Module {
+  val io = new Bundle {
+    val sw = UInt(INPUT, 10)
+    val led = UInt(OUTPUT, 10)
+  }
+
+  val alu = Module(new Alu())
+
+  // Map switches to the ALU input ports
+  alu.io.fn := io.sw(1, 0)
+  alu.io.a := io.sw(5, 2)
+  alu.io.b := io.sw(9, 6)
+
+  // And the result to the LEDs (with 0 extension)
+  io.led := alu.io.result
 }
 
 // Generate the Verilog code by invoking chiselMain() in our main()
 object AluMain {
   def main(args: Array[String]): Unit = {
-    println("Hello")
-    chiselMain(args, () => Module(new Alu()))
+    println("Generating the ALU hardware")
+    chiselMain(args, () => Module(new AluTop()))
   }
 }
 
-// Test the ALU design
+/**
+ * Test the Alu design
+ */
 class AluTester(dut: Alu) extends Tester(dut) {
 
   // This is exhaustive testing, which usually is not possible
@@ -67,9 +90,11 @@ class AluTester(dut: Alu) extends Tester(dut) {
           }
         val res = UInt((result & 0x0f), 4)
 
-        poke(dut.io.sw, (b << 6) + (a << 2) + op)
-        step(1) // is a step for truly combinational circuit needed?
-        expect(dut.io.led, res.litValue())
+        poke(dut.io.fn, op)
+        poke(dut.io.a, a)
+        poke(dut.io.b, b)
+        step(1) // for a truly combinational circuit not really needed
+        expect(dut.io.result, res.litValue())
       }
     }
   }
@@ -77,6 +102,7 @@ class AluTester(dut: Alu) extends Tester(dut) {
 
 object AluTester {
   def main(args: Array[String]): Unit = {
+    println("Testing the ALU")
     chiselMainTest(args, () => Module(new Alu())) {
       f => new AluTester(f)
     }
