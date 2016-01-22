298a98622fb88770520b1d577475e676d026e3ad
Martin Schoeberl <martin@jopdesign.com>
2016-01-22 14:14:03 -0800

    Better signal names for the FIFO

--- examples/src/simple/Fifo.scala
+++ examples/src/simple/Fifo.scala
@@ -11,26 +11,40 @@ import Chisel._
 import Node._
 import scala.collection.mutable.HashMap
 
-class EnqIO(size: Int) extends Bundle {
-  val din = UInt(INPUT, size)
+/*
+ * On signal naming:
+ * 
+ * Alter's FIFO component:
+ * 
+ * data - data in, q - data out, wrreq and rdreq
+ * state: full and empty
+ * 
+ * Xilinx's FIFO component:
+ * din and dout, wr_en, rd_en
+ * state: full and empty
+ * 
+ */
+
+class WriterIO(size: Int) extends Bundle {
   val write = Bool(INPUT)
-  val ready = Bool(OUTPUT) // or use full
+  val full = Bool(OUTPUT)
+  val din = UInt(INPUT, size)
 }
-class DeqIO(size: Int) extends Bundle {
-  val dout = UInt(OUTPUT, size)
+
+class ReaderIO(size: Int) extends Bundle {
   val read = Bool(INPUT)
-  val valid = Bool(OUTPUT)
+  val empty = Bool(OUTPUT)
+  val dout = UInt(OUTPUT, size)
 }
 
 class FifoRegister(size: Int) extends Module {
   val io = new Bundle {
-    val enq = new EnqIO(size)
-    val deq = new DeqIO(size)
+    val enq = new WriterIO(size)
+    val deq = new ReaderIO(size)
   }
 
   val empty :: full :: Nil = Enum(UInt(), 2)
   val stateReg = Reg(init = empty)
-  // TODO: maybe just specify the size and not a reset value
   val dataReg = Reg(init = Bits(0, size))
 
   when(stateReg === empty) {
@@ -41,13 +55,14 @@ class FifoRegister(size: Int) extends Module {
   }.elsewhen(stateReg === full) {
     when(io.deq.read) {
       stateReg := empty
+      dataReg := Bits(0) // just to better see empty slots in the waveform
     }
   }.otherwise {
     // There should not be an otherwise state
   }
 
-  io.enq.ready := (stateReg === empty)
-  io.deq.valid := (stateReg === full)
+  io.enq.full := (stateReg === full)
+  io.deq.empty := (stateReg === empty)
   io.deq.dout := dataReg
 }
 /**
@@ -56,8 +71,8 @@ class FifoRegister(size: Int) extends Module {
  */
 class Fifo(size: Int, depth: Int) extends Module {
   val io = new Bundle {
-    val enq = new EnqIO(size)
-    val deq = new DeqIO(size)
+    val enq = new WriterIO(size)
+    val deq = new ReaderIO(size)
   }
 
   val stage = Module(new FifoRegister(size))
@@ -66,9 +81,9 @@ class Fifo(size: Int, depth: Int) extends Module {
   // We need to use Scala arrays! Vec is only for Data aggregate.
   val buffers = Array.fill(depth) { Module(new FifoRegister(size)) }
   for (i <- 0 until depth - 1) {
-    buffers(i).io.deq.dout <> buffers(i + 1).io.enq.din
-    buffers(i).io.deq.valid <> buffers(i + 1).io.enq.write
-    buffers(i).io.deq.read <> buffers(i + 1).io.enq.ready
+    buffers(i + 1).io.enq.din := buffers(i).io.deq.dout
+    buffers(i + 1).io.enq.write := ~buffers(i).io.deq.empty
+    buffers(i).io.deq.read := ~buffers(i + 1).io.enq.full
   }
   io.enq <> buffers(0).io.enq
   io.deq <> buffers(depth - 1).io.deq
@@ -84,18 +99,18 @@ class FifoTester(dut: Fifo) extends Tester(dut) {
   poke(dut.io.enq.write, 0)
   poke(dut.io.deq.read, 0)
   step(1)
-  peek(dut.io.enq.ready)
+  peek(dut.io.enq.full)
 
   // write into the buffer
   poke(dut.io.enq.din, 0x12)
   poke(dut.io.enq.write, 1)
   step(1)
-  peek(dut.io.enq.ready)
+  peek(dut.io.enq.full)
 
   poke(dut.io.enq.din, 0x34)
   poke(dut.io.enq.write, 0)
   step(1)
-  peek(dut.io.enq.ready)
+  peek(dut.io.enq.full)
 
   // read out
   poke(dut.io.deq.read, 1)
@@ -107,7 +122,7 @@ class FifoTester(dut: Fifo) extends Tester(dut) {
   poke(dut.io.enq.din, 0x56)
   poke(dut.io.enq.write, 1)
   step(1)
-  peek(dut.io.enq.ready)
+  peek(dut.io.enq.full)
   
   for (i <- 0 until 20)
     step(1)
