b9f5b52b63f77fbbc65637aa4ce44acc503091f0
Martin Schoeberl <martin@jopdesign.com>
2016-02-17 17:45:30 -0800

    Better signal names for the Altera JTAG interface

--- examples/src/main/scala/altera/AlteraJtag.scala
+++ examples/src/main/scala/altera/AlteraJtag.scala
@@ -17,15 +17,15 @@ import scala.collection.mutable.HashMap
 class alt_jtag_atlantic extends BlackBox {
   val io = new Bundle {
     val rst_n = UInt(INPUT, 1)
-    val r_dat = UInt(INPUT, 8) // data from FPGA
+    val r_dat = UInt(INPUT, 8) // data from FPGA to host
     val r_val = UInt(INPUT, 1) // data valid
     val r_ena = UInt(OUTPUT, 1) // can write (next) cycle, or FIFO not full?
     val t_dat = UInt(OUTPUT, 8) // data to FPGA
     val t_dav = UInt(INPUT, 1) // ready to receive more data
-    val t_ena = UInt(OUTPUT, 1) // tx data valid
+    val t_ena = UInt(OUTPUT, 1) // tx data valid (tx form host view)
     val t_pause = UInt(OUTPUT, 1) // ???
   }
-  
+
   setVerilogParameters(new VerilogParameters {
     val INSTANCE_ID = 0
     val LOG2_RXFIFO_DEPTH = 3
@@ -47,51 +47,129 @@ class alt_jtag_atlantic extends BlackBox {
 }
 
 /**
- * This is the interface to the Altera JTAG stuff.
- * Chisel does not like a module without IO.
+ * Wrap the Altera Atlantic in a nicer Chisel module.
+ *
+ * See: https://www.altera.com/content/dam/altera-www/global/en_US/pdfs/literature/fs/fs_atlantic.pdf
+ *
+ * Altera talks about "Slave sink indicates it has space for threshold words”.
+ * Could the jtag_atlantic spill out up to 2^LOG2_RXFIFO_DEPTH for a single
+ * dav (rx_ready)?
+ *
  */
-class AlteraJtag(resetSignal: Bool = null) extends Module(_reset = resetSignal) {
+
+class AlteraJtag() extends Module {
+  val io = new Bundle {
+    val txData = UInt(INPUT, 8) // data from FPGA to host
+    val txValid = UInt(INPUT, 1) // data valid
+    val txReady = UInt(OUTPUT, 1) // can write (next) cycle, or FIFO not full?
+
+    val rxData = UInt(OUTPUT, 8) // data from host to FPGA
+    val rxReady = UInt(INPUT, 1) // ready to receive more data
+    val rxValid = UInt(OUTPUT, 1) // rx data valid
+
+  }
+
+  val jtag = Module(new alt_jtag_atlantic())
+
+  jtag.io.rst_n := ~reset
+
+  // Have better names.
+  // Inspired by Tommy and the AXI interface.
+  // Maybe the signals should be Bool?
+  // Altera Atlantic interface (mapped to nicer names)
+
+  // Assuming host is master, FPGA slave
+  //
+  // clock             /--\__/--\__/--\__/--\__/--\__/--\__
+  //
+  // M: ena, txReady:  _______/-----\___________/----------
+  //
+  // S: val, txValid:  _____________/-----------\__________
+  //
+  // S: dat, txData:   XXXXXXXXXXXXXX Data       XXXXXXXXX
+  //
+  // Master requests new data with ena.
+  // Slave holds data until master requests new data.
+  //
+  io.txReady := jtag.io.r_ena
+  jtag.io.r_val := io.txValid
+  jtag.io.r_dat := io.txData
+
+  // Host is master, FPGA slave
+  //
+  // clock             /--\__/--\__/--\__/--\__/--\__/--\__
+  //
+  // S: dav, rxReady:  _______/-----------\________________
+  //
+  // M: ena, rxValid:  _____________/-----------\__________
+  //
+  // M: dat, rxData:   XXXXXXXXXXXXXX Dat X Dat XXXXXXXX
+  //
+  // Slave indicates with dav for space up to 'threshold' words.
+  // Master start sending data (ena, data).
+  // Slave indicates with dav low not having 'threshold' words space.
+  //
+  jtag.io.t_dav := io.rxReady
+  io.rxValid := jtag.io.t_ena
+  io.rxData := jtag.io.t_dat
+
+  // TODO: may having tiny FSM to translate to the simpler AXI interface?
+}
+/**
+ * This is a simple echo component to test the Altera JTAG communication.
+ * Echos a character incremented by 1: 'a' => 'b'
+ */
+class AlteraJtagEcho(resetSignal: Bool = null) extends Module(_reset = resetSignal) {
   val io = new Bundle {
     val led = UInt(OUTPUT, 1)
   }
 
-  val jtag = Module(new alt_jtag_atlantic())
+  val jtag = Module(new AlteraJtag())
 
   val isFullReg = Reg(init = Bool(false))
   val dataReg = Reg(init = UInt(0, 8))
 
   when(!isFullReg) {
-    when(jtag.io.t_ena === UInt(1)) {
-      dataReg := jtag.io.t_dat + UInt(1)
+    when(jtag.io.rxValid === UInt(1)) {
+      dataReg := jtag.io.rxData + UInt(1)
       isFullReg := Bool(true)
     }
   }.otherwise {
-    when(jtag.io.r_ena === UInt(1)) {
+    when(jtag.io.txReady === UInt(1)) {
       isFullReg := Bool(false)
     }
   }
 
-  jtag.io.t_dav := !isFullReg && (jtag.io.t_ena =/= UInt(1))
-  jtag.io.r_val := isFullReg
-  jtag.io.r_dat := dataReg
-  jtag.io.rst_n := ~reset
+  jtag.io.rxReady := !isFullReg && (jtag.io.rxValid =/= UInt(1))
+  jtag.io.txValid := isFullReg
+  jtag.io.txData := dataReg
 
-  val reg = Reg(next = jtag.io.t_dat)
+  // val reg = Reg(next = jtag.io.rxData)
 
-  // Do some blinking to see the FPGA running
-  val CNT_MAX = UInt(24000000 / 2 - 1);
-  val r1 = Reg(init = UInt(0, 25))
-  val blk = Reg(init = UInt(0, 1))
+  // blink on receiving a character
+  val blink = Reg(init = UInt(0, 1))
 
-  r1 := r1 + UInt(1)
-  when(r1 === CNT_MAX) {
-    r1 := UInt(0)
-    blk := ~blk
+  when(!isFullReg && jtag.io.rxValid === UInt(1)) {
+    blink := ~blink;
   }
-  io.led := blk
+
+//  // Do some blinking to see the FPGA running
+//  val CNT_MAX = UInt(24000000 / 2 - 1);
+//  val r1 = Reg(init = UInt(0, 25))
+//  val blk = Reg(init = UInt(0, 1))
+//
+//  r1 := r1 + UInt(1)
+//  when(r1 === CNT_MAX) {
+//    r1 := UInt(0)
+//    blk := ~blk
+//  }
+  io.led := blink
 }
 
-class AlteraJtagTop extends Module {
+/**
+ * A top module to set reset to 0.
+ */
+class AlteraJtagEchoTop extends Module {
   val io = new Bundle {
     val led = UInt(OUTPUT, 1)
   }
@@ -99,13 +177,13 @@ class AlteraJtagTop extends Module {
   // don't use a reset for now
   val resetSignal = Bool(false)
 
-  val jtag = Module(new AlteraJtag(resetSignal))
+  val jtag = Module(new AlteraJtagEcho(resetSignal))
   io <> jtag.io
 }
 
-object AlteraJtag {
+object AlteraJtagEcho {
   def main(args: Array[String]): Unit = {
     chiselMain(Array[String]("--backend", "v", "--targetDir", "generated"),
-      () => Module(new AlteraJtagTop()))
+      () => Module(new AlteraJtagEchoTop()))
   }
 }
