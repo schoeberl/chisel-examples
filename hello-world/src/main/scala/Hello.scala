bd3defaf4a78897355dc1d69a7f02c491712e34b
Martin Schoeberl <martin@jopdesign.com>
2019-07-22 14:58:25 +0200

    hello: use the reset

--- hello-world/src/main/scala/Hello.scala
+++ hello-world/src/main/scala/Hello.scala
@@ -20,9 +20,9 @@ class Hello extends Module {
     val led = Output(UInt(1.W))
   })
   val CNT_MAX = (50000000 / 2 - 1).U;
-  
-  val cntReg = Reg(UInt(32.W))
-  val blkReg = Reg(UInt(1.W))
+
+  val cntReg = RegInit(0.U(32.W))
+  val blkReg = RegInit(0.U(1.W))
 
   cntReg := cntReg + 1.U
   when(cntReg === CNT_MAX) {
