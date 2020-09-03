d6aaf18568c3282431086b92b8721045724c19b6
Martin Schoeberl <martin@jopdesign.com>
2020-09-03 13:52:39 +0200

    hello: better simulation of the LED

--- hello-world/src/test/scala/HelloSpec.scala
+++ hello-world/src/test/scala/HelloSpec.scala
@@ -7,11 +7,16 @@ class HelloSpec extends FlatSpec with Matchers {
     chisel3.iotesters.Driver(() => new Hello()) { c =>
       new PeekPokeTester(c) {
 
+        var ledStatus = -1
         println("Start the blinking LED")
         for (i <- 0 until 100) {
           step(10000)
-          val ch = if (peek(c.io.led) == 0) 'o' else '*'
-          printf(ch + "\r")
+          val ledNow = peek(c.io.led).toInt
+          val s = if (ledNow == 0) "o" else "*"
+          if (ledStatus != ledNow) {
+            System.out.println(s)
+            ledStatus = ledNow
+          }
         }
         println("\nEnd the blinking LED")
       }
